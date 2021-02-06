3e0ec07510211a633cc1f545eb221027cd09c0aa
Bailey <erringtonbusiness@gmail.com>
2021-02-06 18:56:33 +0000

    Initial commit

---
+++ src/main/java/Zone/Zone.java
@@ -0,0 +1,69 @@
+package Zone;
+
+import basemod.AutoAdd;
+import basemod.BaseMod;
+import basemod.ModLabeledToggleButton;
+import basemod.ModPanel;
+import basemod.interfaces.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.mod.stslib.Keyword;
+import com.evacipated.cardcrawl.modthespire.Loader;
+import com.evacipated.cardcrawl.modthespire.lib.SpireConfig;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.localization.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+@SpireInitializer
+public class Zone implements
+        EditStringsSubscriber,
+        EditKeywordsSubscriber {
+
+    private static String modID = "zone";
+    public static String getModID() { return modID; }
+    public static String makeID(String idText) { return getModID() + ":" + idText; }
+
+    public Zone() { BaseMod.subscribe(this); }
+
+    public static String makePath(String resourcePath) { return modID + "Resources/" + resourcePath; }
+    public static String makeImagePath(String resourcePath) { return modID + "Resources/images/" + resourcePath; }
+    public static String makeRelicPath(String resourcePath) { return modID + "Resources/images/relics/" + resourcePath; }
+    public static String makeOutlinePath(String resourcePath) { return modID + "Resources/images/relics/outline/" + resourcePath; }
+    public static String makePowerPath(String resourcePath) { return modID + "Resources/images/powers/" + resourcePath; }
+    public static String makeCardPath(String resourcePath) { return modID + "Resources/images/cards/" + resourcePath; }
+    public static String makeCharacterPath(String resourcePath) { return modID + "Resources/images/character/" + resourcePath; }
+    public static String makeEffectPath(String resourcePath) { return modID + "Resources/images/effects/" + resourcePath; }
+
+    public static void initialize() { Zone zone = new Zone(); }
+
+    @Override
+    public void receiveEditStrings() {
+        BaseMod.loadCustomStringsFile(CardStrings.class, getModID() + "Resources/localization/eng/Cardstrings.json");
+        BaseMod.loadCustomStringsFile(RelicStrings.class, getModID() + "Resources/localization/eng/Relicstrings.json");
+        BaseMod.loadCustomStringsFile(CharacterStrings.class, getModID() + "Resources/localization/eng/Charstrings.json");
+        BaseMod.loadCustomStringsFile(PowerStrings.class, getModID() + "Resources/localization/eng/Powerstrings.json");
+        BaseMod.loadCustomStringsFile(UIStrings.class, getModID() + "Resources/localization/eng/Uistrings.json");
+    }
+
+    @Override
+    public void receiveEditKeywords() {
+        Gson gson = new Gson();
+        String json = Gdx.files.internal(getModID() + "Resources/localization/eng/Keywordstrings.json").readString(String.valueOf(StandardCharsets.UTF_8));
+        com.evacipated.cardcrawl.mod.stslib.Keyword[] keywords = gson.fromJson(json, com.evacipated.cardcrawl.mod.stslib.Keyword[].class);
+
+        if (keywords != null) {
+            for (Keyword keyword : keywords) {
+                BaseMod.addKeyword(modID, keyword.PROPER_NAME, keyword.NAMES, keyword.DESCRIPTION);
+            }
+        }
+    }
+
+}
+
---
+++ src/main/java/Zone/util/RelicInfo.java
@@ -0,0 +1,21 @@
+package Zone.util;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import static Zone.Shameimaru.makeOutlinePath;
+import static Zone.Shameimaru.makeRelicPath;
+
+public class RelicInfo {
+    public String relicName;
+    public AbstractRelic.RelicTier relicTier;
+    public AbstractRelic.LandingSound sfx;
+    public RelicInfo(String relicName, AbstractRelic.RelicTier relicTier, AbstractRelic.LandingSound sfx)
+    {
+        this.relicName = relicName;
+        this.relicTier = relicTier;
+        this.sfx = sfx;
+    }
+    public Texture loadRelicImage(String name){ return TexLoader.getTexture(makeRelicPath(name + ".png")); }
+    public Texture loadOutlineImage(String name){ return TexLoader.getTexture(makeOutlinePath(name + ".png")); }
+}
