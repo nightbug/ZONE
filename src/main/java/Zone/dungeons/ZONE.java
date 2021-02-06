b6d409a8c0afa3ff54aae1ad231aa4ee70cc682b
Bailey <erringtonbusiness@gmail.com>
2021-02-06 22:40:30 +0000

    Background Assets

---
+++ src/main/java/Zone/dungeons/ZONE.java
@@ -0,0 +1,105 @@
+package Zone.dungeons;
+
+import Zone.dungeons.scenes.ZONE_BG;
+import actlikeit.dungeons.CustomDungeon;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.monsters.MonsterInfo;
+import com.megacrit.cardcrawl.saveAndContinue.SaveFile;
+import com.megacrit.cardcrawl.scenes.AbstractScene;
+
+import java.util.ArrayList;
+
+import static Zone.Zone.makeID;
+
+public class ZONE extends actlikeit.dungeons.CustomDungeon {
+
+    public static String ID = makeID(ZONE.class.getSimpleName());
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String[] TEXT = uiStrings.TEXT;
+    public static final String NAME = TEXT[0];
+
+    public ZONE() {
+        super(NAME, ID, "images/ui/event/panel.png", false, 3, 12, 10);
+    }
+
+    public ZONE(CustomDungeon cd, AbstractPlayer p, ArrayList<String> emptyList) {
+        super(cd, p, emptyList);
+    }
+
+    public ZONE(CustomDungeon cd, AbstractPlayer p, SaveFile saveFile) {
+        super(cd, p, saveFile);
+    }
+
+    @Override
+    public AbstractScene DungeonScene() { return new ZONE_BG(); }
+
+    protected void initializeLevelSpecificChances() {
+        shopRoomChance = 0.05F;
+        restRoomChance = 0.12F;
+        treasureRoomChance = 0.0F;
+        eventRoomChance = 0.25F;
+        eliteRoomChance = 0.08F;
+        smallChestChance = 50;
+        mediumChestChance = 33;
+        largeChestChance = 17;
+        commonRelicChance = 50;
+        uncommonRelicChance = 33;
+        rareRelicChance = 17;
+        colorlessRareChance = 0.3F;
+        cardUpgradedChance = 0.0F;
+    }
+
+    @Override
+    public String getBodyText() {
+        return TEXT[2];
+    }
+
+    @Override
+    public String getOptionText() {
+        return TEXT[3];
+    }
+
+    @Override
+    protected void generateMonsters() {
+        generateWeakEnemies(weakpreset);
+        generateStrongEnemies(strongpreset);
+        generateElites(elitepreset);
+    }
+
+    @Override
+    protected void generateWeakEnemies(int count) {
+        ArrayList<MonsterInfo> monsters = new ArrayList();
+        MonsterInfo.normalizeWeights(monsters);
+        this.populateMonsterList(monsters, count, false);
+    }
+    @Override
+    protected void generateStrongEnemies(int count) {
+        ArrayList<MonsterInfo> monsters = new ArrayList();
+        MonsterInfo.normalizeWeights(monsters);
+        this.populateFirstStrongEnemy(monsters, this.generateExclusions());
+        this.populateMonsterList(monsters, count, false);
+    }
+    @Override
+    protected void generateElites(int count) {
+        ArrayList<MonsterInfo> monsters = new ArrayList();
+        MonsterInfo.normalizeWeights(monsters);
+        this.populateMonsterList(monsters, count, true);
+    }
+    @Override
+    protected ArrayList<String> generateExclusions() {
+        ArrayList<String> retVal = new ArrayList();
+        return retVal;
+    }
+
+    @Override
+    protected void initializeShrineList() {
+        //No shrines
+    }
+
+    @Override
+    protected void initializeEventList() {
+        // Events are added via BaseMod in GensokyoMod.receivePostInitialize()
+    }
+}
---
+++ src/main/java/Zone/dungeons/scenes/ZONE_BG.java
@@ -0,0 +1,92 @@
+package Zone.dungeons.scenes;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.CampfireUI;
+import com.megacrit.cardcrawl.rooms.MonsterRoomBoss;
+import com.megacrit.cardcrawl.rooms.ShopRoom;
+import com.megacrit.cardcrawl.scenes.AbstractScene;
+
+public class ZONE_BG extends AbstractScene {
+
+    private static Texture topBar;
+    private TextureAtlas.AtlasRegion bg;
+    private TextureAtlas.AtlasRegion fg;
+    private TextureAtlas.AtlasRegion ceil;
+    private TextureAtlas.AtlasRegion fgGlow;
+    private TextureAtlas.AtlasRegion floor;
+    private TextureAtlas.AtlasRegion mg1;
+    private Texture campfirebg;
+    private Texture campfire;
+    private Texture fire;
+
+    public ZONE_BG() {
+        super("zoneResources/images/scene/atlas.atlas");
+        this.bg = this.atlas.findRegion("mod/bg_bt_g");
+        this.ambianceName = "AMBIANCE_CITY";
+        this.fadeInAmbiance();
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Override
+    public void randomizeScene() {
+    }
+
+    @Override
+    public void nextRoom(AbstractRoom room) {
+        super.nextRoom(room);
+        this.randomizeScene();
+        if (room instanceof MonsterRoomBoss) { CardCrawlGame.music.silenceBGM(); }
+        /*
+        else if (room instanceof ShopRoom) { this.bg = this.atlas.findRegion("mod/shop"); }
+        else {
+
+        }
+
+         */
+        this.bg = this.atlas.findRegion("mod/bg_bt_g");
+        this.fadeInAmbiance();
+    }
+
+    @Override
+    public void renderCombatRoomBg(SpriteBatch sb) {
+        sb.setColor(Color.WHITE.cpy());
+        this.renderAtlasRegionIf(sb, bg, true);
+        sb.setBlendFunction(Gdx.gl20.GL_SRC_ALPHA, Gdx.gl20.GL_ONE_MINUS_SRC_ALPHA);
+        //this.renderAtlasRegionIf(sb, this.floor, true);
+        // this.renderAtlasRegionIf(sb, this.ceil, true);
+        //this.renderAtlasRegionIf(sb, this.mg1, true);
+    }
+
+    @Override
+    public void renderCombatRoomFg(SpriteBatch sb) {
+        sb.setColor(Color.WHITE.cpy());
+        sb.setColor(Color.WHITE.cpy());
+        // this.renderAtlasRegionIf(sb, this.fg, true);
+        // sb.setBlendFunction(Gdx.gl20.GL_SRC_ALPHA, Gdx.gl20.GL_ONE);
+        // this.renderAtlasRegionIf(sb, this.fgGlow, true);
+        // sb.setBlendFunction(Gdx.gl20.GL_SRC_ALPHA, Gdx.gl20.GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    @Override
+    public void renderCampfireRoom(SpriteBatch sb) {
+        sb.setColor(Color.WHITE);
+        this.renderAtlasRegionIf(sb, this.campfireBg, true);
+        sb.setBlendFunction(Gdx.gl20.GL_SRC_ALPHA, Gdx.gl20.GL_ONE);
+        sb.setColor(new Color(1.0f, 1.0f, 1.0f, MathUtils.cosDeg(System.currentTimeMillis() / 3L % 360L) / 10.0f + 0.8f));
+        this.renderQuadrupleSize(sb, this.campfireGlow, !CampfireUI.hidden);
+        sb.setBlendFunction(Gdx.gl20.GL_SRC_ALPHA, Gdx.gl20.GL_ONE_MINUS_SRC_ALPHA);
+        sb.setColor(Color.WHITE);
+        this.renderAtlasRegionIf(sb, this.campfireKindling, true);
+    }
+}
